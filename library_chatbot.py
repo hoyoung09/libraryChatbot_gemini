00ef848340518b63f5826777fbc90b0e7682634f
hoyoung09 <ksa09_250311@gclass.ice.go.kr>
2025-11-15 10:04:21 +0900

    Update library_chatbot.py

--- library_chatbot.py
+++ library_chatbot.py
@@ -81,7 +81,8 @@ def get_vectorstore(_docs):
 # PDF 문서 로드-벡터 DB 저장-검색기-히스토리 모두 합친 Chain 구축
 @st.cache_resource
 def initialize_components(selected_model):
-    file_path = r"/mount/src/librarychatbot_gemini/[챗봇프로그램및실습] 부경대학교 규정집.pdf"
+    file_path = r"/mount/src/librarychatbot_gemini/★ 해양 플라스틱 저감 종합대책(배포용).pdf"
+    file_path = r"/mount/src/librarychatbot_gemini/★ 해양 플라스틱 저감 종합대책(배포용).pdf"
     pages = load_and_split_pdf(file_path)
     vectorstore = get_vectorstore(pages)
     retriever = vectorstore.as_retriever()
